25cbe1d1a986c4d9409908452d7ae7ee92d3987c
ktncktnc <doremon6a1@gmail.com>
2021-08-23 15:38:15 +0700

    fix read img

--- source/utils.py
+++ source/utils.py
@@ -20,9 +20,11 @@ def adjust_shape(I, s):
 
 def read_sentinel_img(path, NORMALISE_IMGS = True):
     im_name = os.listdir(path)[0][:-7]
-    r = io.imread(path + im_name + "B04.tif")
-    g = io.imread(path + im_name + "B03.tif")
-    b = io.imread(path + im_name + "B02.tif")
+    path_img_name = os.path.join(path, im_name)
+
+    r = io.imread(os.path.join(path_img_name, "B04.tif"))
+    g = io.imread(os.path.join(path_img_name, "B03.tif"))
+    b = io.imread(os.path.join(path_img_name, "B02.tif"))
     
     I = np.stack((r,g,b),axis=2).astype('float')
     
@@ -34,10 +36,12 @@ def read_sentinel_img(path, NORMALISE_IMGS = True):
 def read_sentinel_img_4(path, NORMALISE_IMGS = True):
     """Read cropped Sentinel-2 image: RGB and NIR bands."""
     im_name = os.listdir(path)[0][:-7]
-    r = io.imread(path + im_name + "B04.tif")
-    g = io.imread(path + im_name + "B03.tif")
-    b = io.imread(path + im_name + "B02.tif")
-    nir = io.imread(path + im_name + "B08.tif")
+    path_img_name = os.path.join(path, im_name)
+
+    r = io.imread(os.path.join(path_img_name, "B04.tif"))
+    g = io.imread(os.path.join(path_img_name, "B03.tif"))
+    b = io.imread(os.path.join(path_img_name, "B02.tif"))
+    nir = io.imread(os.path.join(path_img_name, "B08.tif"))
     
     I = np.stack((r,g,b,nir),axis=2).astype('float')
     
@@ -49,19 +53,20 @@ def read_sentinel_img_4(path, NORMALISE_IMGS = True):
 def read_sentinel_img_leq20(path, NORMALISE_IMGS = True):
     """Read cropped Sentinel-2 image: bands with resolution less than or equals to 20m."""
     im_name = os.listdir(path)[0][:-7]
+    path_img_name = os.path.join(path, im_name)
     
-    r = io.imread(path + im_name + "B04.tif")
+    r = io.imread(os.path.join(path_img_name, "B04.tif"))
     s = r.shape
-    g = io.imread(path + im_name + "B03.tif")
-    b = io.imread(path + im_name + "B02.tif")
-    nir = io.imread(path + im_name + "B08.tif")
+    g = io.imread(os.path.join(path_img_name, "B03.tif"))
+    b = io.imread(os.path.join(path_img_name, "B02.tif"))
+    nir = io.imread(os.path.join(path_img_name, "B08.tif"))
     
-    ir1 = adjust_shape(zoom(io.imread(path + im_name + "B05.tif"),2),s)
-    ir2 = adjust_shape(zoom(io.imread(path + im_name + "B06.tif"),2),s)
-    ir3 = adjust_shape(zoom(io.imread(path + im_name + "B07.tif"),2),s)
-    nir2 = adjust_shape(zoom(io.imread(path + im_name + "B8A.tif"),2),s)
-    swir2 = adjust_shape(zoom(io.imread(path + im_name + "B11.tif"),2),s)
-    swir3 = adjust_shape(zoom(io.imread(path + im_name + "B12.tif"),2),s)
+    ir1 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B05.tif")),2),s)
+    ir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B06.tif")),2),s)
+    ir3 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B07.tif")),2),s)
+    nir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name "B8A.tif")),2),s)
+    swir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B11.tif")),2),s)
+    swir3 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B12.tif")),2),s)
     
     I = np.stack((r,g,b,nir,ir1,ir2,ir3,nir2,swir2,swir3),axis=2).astype('float')
     
@@ -73,23 +78,24 @@ def read_sentinel_img_leq20(path, NORMALISE_IMGS = True):
 def read_sentinel_img_leq60(path, NORMALISE_IMGS = True):
     """Read cropped Sentinel-2 image: all bands."""
     im_name = os.listdir(path)[0][:-7]
+    path_img_name = os.path.join(path, im_name)
     
-    r = io.imread(path + im_name + "B04.tif")
+    r = io.imread(os.path.join(path_img_name, "B04.tif"))
     s = r.shape
-    g = io.imread(path + im_name + "B03.tif")
-    b = io.imread(path + im_name + "B02.tif")
-    nir = io.imread(path + im_name + "B08.tif")
+    g = io.imread(os.path.join(path_img_name, "B03.tif"))
+    b = io.imread(os.path.join(path_img_name, "B02.tif"))
+    nir = io.imread(os.path.join(path_img_name, "B08.tif"))
     
-    ir1 = adjust_shape(zoom(io.imread(path + im_name + "B05.tif"),2),s)
-    ir2 = adjust_shape(zoom(io.imread(path + im_name + "B06.tif"),2),s)
-    ir3 = adjust_shape(zoom(io.imread(path + im_name + "B07.tif"),2),s)
-    nir2 = adjust_shape(zoom(io.imread(path + im_name + "B8A.tif"),2),s)
-    swir2 = adjust_shape(zoom(io.imread(path + im_name + "B11.tif"),2),s)
-    swir3 = adjust_shape(zoom(io.imread(path + im_name + "B12.tif"),2),s)
+    ir1 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B05.tif")),2),s)
+    ir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B06.tif")),2),s)
+    ir3 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B07.tif")),2),s)
+    nir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B8A.tif")),2),s)
+    swir2 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B11.tif")),2),s)
+    swir3 = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B12.tif")),2),s)
     
-    uv = adjust_shape(zoom(io.imread(path + im_name + "B01.tif"),6),s)
-    wv = adjust_shape(zoom(io.imread(path + im_name + "B09.tif"),6),s)
-    swirc = adjust_shape(zoom(io.imread(path + im_name + "B10.tif"),6),s)
+    uv = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B01.tif")),6),s)
+    wv = adjust_shape(zoom(io.imread(os.path.join(path_img_name,"B09.tif"),6),s)
+    swirc = adjust_shape(zoom(io.imread(os.path.join(path_img_name, "B10.tif")),6),s)
     
     I = np.stack((r,g,b,nir,ir1,ir2,ir3,nir2,swir2,swir3,uv,wv,swirc),axis=2).astype('float')
     
