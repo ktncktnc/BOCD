346c6a36bb1d2704405cb72c48e03a3f72db3762
ktncktnc <doremon6a1@gmail.com>
2021-09-29 22:22:01 +0700

    add random dataset

--- source/dataset.py
+++ source/dataset.py
@@ -6,6 +6,7 @@ import os
 import torch
 from torch.utils.data import Dataset
 from math import floor, ceil, sqrt, exp
+import random
 
 
 
@@ -37,7 +38,7 @@ class CDDataset(Dataset):
         else:
             fname = 'test.txt'
         
-#         print(path + fname)
+        #print(path + fname)
         self.names = read_csv(os.path.join(path, fname)).columns
         self.n_imgs = self.names.shape[0]
         
@@ -82,8 +83,106 @@ class CDDataset(Dataset):
                     
         self.weights = [FP_MODIFIER * 2 * true_pix / n_pix, 2 * (n_pix - true_pix) / n_pix]
         
-        
+    def get_img(self, im_name):
+        return {
+            'I1': self.imgs_1[im_name], 
+            'I2': self.imgs_2[im_name], 
+            'label': self.change_maps[im_name]
+        }
 
+    def __len__(self):
+        return self.n_patches
+
+    def __getitem__(self, idx):
+        current_patch_coords = self.patch_coords[idx]
+        im_name = current_patch_coords[0]
+        limits = current_patch_coords[1]
+        centre = current_patch_coords[2]
+        
+        I1 = self.imgs_1[im_name][:, limits[0]:limits[1], limits[2]:limits[3]]
+        I2 = self.imgs_2[im_name][:, limits[0]:limits[1], limits[2]:limits[3]]
+        
+        label = self.change_maps[im_name][limits[0]:limits[1], limits[2]:limits[3]]
+        label = torch.from_numpy(1*np.array(label)).float()
+        
+        sample = {'I1': I1, 'I2': I2, 'label': label}
+        
+        #if self.transform:
+        #    sample = self.transform(sample)
+
+        return sample
+
+
+class RandomCropCDDataset(Dataset):
+    """
+    Change Detection dataset class, used for both training and test data.
+        
+    Args:
+        path (string): Path to the OSCD dataset directory
+        train (boolean: get train or test dataset
+        transform (callable, optional): Optional transform to be applied
+            on a sample.
+    """
+
+    def __init__(self, path, train = True, patch_side = 96, nums_crop = 3000, transform=None, img_type = 0, normalize = True, FP_MODIFIER = 10):
+
+        # basics
+        self.transform = transform
+        self.path = path
+        self.patch_side = patch_side
+
+        if train:
+            fname = 'train.txt'
+        else:
+            fname = 'test.txt'
+        
+        #print(path + fname)
+        self.names = read_csv(os.path.join(path, fname)).columns
+        self.n_imgs = self.names.shape[0]
+
+        nums_crop_per_img = nums_crop // self.n_imgs
+        
+        n_pix = 0
+        true_pix = 0
+        
+        
+        # load images
+        self.imgs_1 = {}
+        self.imgs_2 = {}
+        self.change_maps = {}
+        self.n_patches_per_image = {}
+        self.n_patches = 0
+        self.patch_coords = []
+        for im_name in tqdm(self.names):
+            # load and store each image
+            I1, I2, cm = read_sentinel_img_trio(os.path.join(self.path, im_name), img_type, normalize)
+            self.imgs_1[im_name] = reshape_for_torch(I1)
+            self.imgs_2[im_name] = reshape_for_torch(I2)
+            self.change_maps[im_name] = cm
+            
+            s = cm.shape
+            n_pix += np.prod(s)
+            true_pix += cm.sum()
+            
+            # calculate the number of patches
+            s = self.imgs_1[im_name].shape
+            self.n_patches_per_image[im_name] = nums_crop_per_img
+            self.n_patches += nums_crop_per_img
+
+            n1 = ceil(s[1] - self.patch_side + 1)
+            n2 = ceil(s[1] - self.patch_side + 1)
+
+            for i in range(nums_crop_per_img):
+                n1_random = random.randint(0, n1)
+                n2_random = random.randint(0, n2)
+
+                current_patch_coords = (im_name, 
+                                    [n1_random, n1_random + self.patch_side, n2_random, n2_random + self.patch_side],
+                                    [n1_random + self.patch_side//2, n2_random + self.patch_side//2])
+                self.patch_coords.append(current_patch_coords)
+                    
+        self.weights = [FP_MODIFIER * 2 * true_pix / n_pix, 2 * (n_pix - true_pix) / n_pix]
+        
     def get_img(self, im_name):
         return {
             'I1': self.imgs_1[im_name], 
